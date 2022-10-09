c39cea4e4f386d44dcaaeb17b059926752f96952
laserzz <82984448+laserzz@users.noreply.github.com>
2022-10-09 01:01:23 -0700

    init

---
+++ main.py
@@ -0,0 +1,52 @@
+import os
+import time
+from colorama import Fore
+from utils.create import Create
+
+class Radish:
+    def run():
+        print("Welcome To..")
+        time.sleep(2)
+
+        print(Fore.RED + """
+                                  ___                     ___        ___  
+                                 (   )  .-.              (   )      (   ) 
+         ___ .-.      .---.    .-.| |  ( __)     .--.     | | .-.    | |  
+        (   )   \    / .-, \  /   \ |  (''")   /  _  \    | |/   \   | |  
+         | ' .-. ;  (__) ; | |  .-. |   | |   . .' `. ;   |  .-. .   | |  
+         |  / (___)   .'`  | | |  | |   | |   | '   | |   | |  | |   | |  
+         | |         / .'| | | |  | |   | |   _\_`.(___)  | |  | |   | |  
+         | |        | /  | | | |  | |   | |  (   ). '.    | |  | |   | |  
+         | |        ; |  ; | | '  | |   | |   | |  `\ |   | |  | |   |_|  
+         | |        ' `-'  | ' `-'  /   | |   ; '._,' '   | |  | |   .-.  
+        (___)       `.__.'_.  `.__,'   (___)   '.___.'   (___)(___) (   ) 
+                                                                     '-'  
+
+        """)
+        time.sleep(1)
+
+        while True:
+            udir = input("Enter a directory for setup. ")
+            if not os.path.isdir(udir):
+                print("Not a directory.")
+
+            else:
+                break
+
+        folders = []
+        
+        while True:
+            uf = input("Enter a subfolder to add. \n Type e to exit. ")
+            if uf.lower() == 'e':
+                break
+
+            folders.append(uf)
+
+        ext = input("Enter a file extension. ")
+
+        cre = Create(dirs=folders)
+
+        cre.make_dirs(dir=udir)
+        cre.make_base_files(dir=udir, ext=ext)
+
+Radish.run()
---
+++ utils/create.py
@@ -0,0 +1,21 @@
+import os
+
+class Create:
+    def __init__(self, dirs:list):
+        self.dirs = dirs
+
+    def make_dirs(self, dir):
+        for d in self.dirs:
+            os.makedirs(f"{dir}/{d}")
+
+    def make_base_files(self, dir:str, ext:str):
+        with open(f"{dir}/.gitignore", 'w') as f:
+            f.write("Write your files to ignore in commits here.")
+            f.close()
+        
+        with open(f"{dir}/main.{ext}", 'w') as f:
+            f.close()
+
+        with open(f"{dir}/readme.md", 'w') as f:
+            f.write("### Create a README here.")
+            f.close()
